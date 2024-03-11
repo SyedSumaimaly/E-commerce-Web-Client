34fcb2384a9efef29d7cba26a545dd313659380b
shoaibsaleem0299 <shoaib0299@gmail.com>
2024-03-11 17:16:59 +0500

    start category section

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Section2 from './components/Categories'
 import reportWebVitals from './reportWebVitals';
+import FlowbiteCard from './components/FlowbiteCard';
+import Categories from './components/Categories';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <FlowbiteCard /> */}
+    {/* <Section2 /> */}
+    <Categories />
   </React.StrictMode>
 );
 
